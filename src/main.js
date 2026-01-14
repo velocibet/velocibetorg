0c816bacb1e04f15793344d7b265933d3f152174
velocibet <honoka4h@gmail.com>
2026-01-14 23:33:28 +0900

    add sidebar and new gallery

--- src/main.js
+++ src/main.js
@@ -1,6 +1,17 @@
 const audio = document.querySelector("#bgm");
 const image = document.querySelector("#hidden-image");
 const visitorCountElement = document.querySelector("#visitor-count");
+
+const navBtn1 = document.querySelector("#nav-btn-1");
+const navBtn2 = document.querySelector("#nav-btn-2");
+const navBtn3 = document.querySelector("#nav-btn-3");
+
+const content1 = document.querySelector(".content-container-01");
+const content2 = document.querySelector(".content-container-02");
+const content3 = document.querySelector(".content-container-03");
+
+const imageElements = document.querySelectorAll("#imageElement");
+
 let played = false;
 
 const API_URL = import.meta.env.VITE_API_URL;
@@ -36,7 +47,30 @@ function updateVisitorCount(count) {
     visitorCountElement.innerHTML = String(count);
 }
 
+function navigationTo(page) {
+    content1.style.display = "none";
+    content2.style.display = "none";
+    content3.style.display = "none";
+
+    if (page === 1) {
+        content1.style.display = "flex";
+    } else if (page === 2) {
+        content2.style.display = "flex";
+    } else if (page === 3) {
+        content3.style.display = "flex";
+    }
+}
+
+imageElements.forEach(ele => {
+    ele.addEventListener("click", () => {
+        window.open(ele.src, "_blank");
+    })
+})
+
 image.addEventListener("click", handleImageClick);
 document.addEventListener("click", handleAudioPlay);
+navBtn1.addEventListener("click", () => navigationTo(1));
+navBtn2.addEventListener("click", () => navigationTo(2));
+navBtn3.addEventListener("click", () => navigationTo(3));
 
 await getVisitorCount();
